69af9c002168fd68b032a6ca6da6f5a1eab34f23
Robin Tail <robin_tail@me.com>
2022-11-05 23:14:55 +0100

    Draft implementation.

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import type {SxProps} from '@mui/material';
+
+/**
+ * @desc Combines multiple SxProps
+ * @author Anna Bocharova
+ * @link https://stackoverflow.com/a/71533846
+ * @link https://mui.com/system/getting-started/the-sx-prop/#array-values
+ * @link https://mui.com/system/getting-started/the-sx-prop/#passing-the-sx-prop
+ * @requires SxProps from MUI 5.1.0 or higher
+ * @see https://github.com/mui/material-ui/releases/tag/v5.1.0
+ * @link https://github.com/mui/material-ui/pull/29297
+ */
+export const mergeSx = <T extends object>(...styles: (SxProps<T> | undefined)[]): SxProps<T> => {
+  const ensureArray = (sx: SxProps<T>) => (Array.isArray(sx) ? sx : [sx]);
+  return styles.reduce<SxProps<T>>((agg, sx) => [...ensureArray(agg), ...ensureArray(sx || [])], []);
+};
+
+export const sxMerge = mergeSx;
+export default mergeSx;
