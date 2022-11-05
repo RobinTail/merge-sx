134f1b74993d24d8adf1f36b90fbc7f1745f1b83
Robin Tail <robin_tail@me.com>
2022-11-06 00:10:51 +0100

    More organized tests.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,47 +2,74 @@ import type { SxProps, Theme } from "@mui/material";
 import { mergeSx } from "./index";
 
 describe("mergeSx()", () => {
-  it("combines two SxProps", () => {
-    expect(mergeSx({ mt: 1 }, { mb: 1 })).toEqual([{ mt: 1 }, { mb: 1 }]);
+  describe("simple cases", () => {
+    it("combines two SxProps", () => {
+      expect(mergeSx({ mt: 1 }, { mb: 1 })).toEqual([{ mt: 1 }, { mb: 1 }]);
+    });
+
+    it("combines three SxProps", () => {
+      expect(mergeSx({ mt: 1 }, { mb: 1 }, { ml: 1 })).toEqual([
+        { mt: 1 },
+        { mb: 1 },
+        { ml: 1 },
+      ]);
+    });
   });
 
-  it("combines three SxProps", () => {
-    expect(mergeSx({ mt: 1 }, { mb: 1 }, { ml: 1 })).toEqual([
-      { mt: 1 },
-      { mb: 1 },
-      { ml: 1 },
-    ]);
+  describe("variations of SxProps", () => {
+    it("handles functions", () => {
+      const sx1: SxProps = { mt: 1 };
+      const sx2: SxProps<Theme> = (theme) => ({ mb: theme.spacing(1) });
+      expect(mergeSx(sx1, sx2)).toEqual([sx1, sx2]);
+    });
+
+    it("handles arrays of SxProps", () => {
+      expect(mergeSx({ mt: 1 }, [{ mb: 1 }, { ml: 1 }])).toEqual([
+        { mt: 1 },
+        { mb: 1 },
+        { ml: 1 },
+      ]);
+    });
+
+    it("handles null", () => {
+      expect(mergeSx({ mt: 1 }, null, { mb: 1 })).toEqual([
+        { mt: 1 },
+        { mb: 1 },
+      ]);
+    });
+
+    it("handles false in array", () => {
+      expect(mergeSx({ mt: 1 }, [false, { ml: 1 }])).toEqual([
+        { mt: 1 },
+        false,
+        { ml: 1 },
+      ]);
+    });
   });
 
-  it("combines different kinds of SxProps", () => {
-    const sx1: SxProps = { mt: 1 };
-    const sx2: SxProps<Theme> = (theme) => ({ mb: theme.spacing(1) });
-    expect(mergeSx(sx1, sx2)).toEqual([sx1, sx2]);
+  describe("edge cases", () => {
+    it("handles a single SxProps", () => {
+      expect(mergeSx({ mt: 1 })).toEqual([{ mt: 1 }]);
+    });
+
+    it("handles zero arguments", () => {
+      expect(mergeSx()).toEqual([]);
+    });
   });
 
-  it("handles a single SxProps", () => {
-    expect(mergeSx({ mt: 1 })).toEqual([{ mt: 1 }]);
-  });
+  describe("conditional type cases", () => {
+    it("removes false from the top level", () => {
+      expect(mergeSx({ mt: 1 }, false, { mb: 1 })).toEqual([
+        { mt: 1 },
+        { mb: 1 },
+      ]);
+    });
 
-  it("handles zero arguments", () => {
-    expect(mergeSx()).toEqual([]);
-  });
-
-  it("handles null", () => {
-    expect(mergeSx({ mt: 1 }, null, { mb: 1 })).toEqual([{ mt: 1 }, { mb: 1 }]);
-  });
-
-  it("handles false", () => {
-    expect(mergeSx({ mt: 1 }, false, { mb: 1 })).toEqual([
-      { mt: 1 },
-      { mb: 1 },
-    ]);
-  });
-
-  it("handles undefined", () => {
-    expect(mergeSx({ mt: 1 }, undefined, { mb: 1 })).toEqual([
-      { mt: 1 },
-      { mb: 1 },
-    ]);
+    it("removes undefined", () => {
+      expect(mergeSx({ mt: 1 }, undefined, { mb: 1 })).toEqual([
+        { mt: 1 },
+        { mb: 1 },
+      ]);
+    });
   });
 });
