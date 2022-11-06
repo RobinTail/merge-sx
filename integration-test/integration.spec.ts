dafd531c60f30ab363b3a27d5a32f99325983db1
Robin Tail <robin_tail@me.com>
2022-11-06 11:36:47 +0100

    Sample integration test on the latest MUI.

---
+++ integration-test/integration.spec.ts
@@ -0,0 +1,17 @@
+import type { SxProps, Theme } from "@mui/material";
+import mergeSx from "merge-sx";
+
+describe("Integration", () => {
+  test("mergeSx() operates SxProps", () => {
+    const sx1: SxProps<Theme> = { mt: 1 };
+    const sx2: SxProps<Theme> = (theme) => ({
+      mb: theme.spacing(1),
+    });
+    const sx3: SxProps<Theme> = [{ ml: 1 }, { mr: 1 }];
+    const isMobile = false;
+    const props: { sx?: SxProps<Theme> } = {};
+
+    expect(mergeSx(sx1, sx2, sx3)).toEqual([sx1, sx2, sx3[0], sx3[1]]);
+    expect(mergeSx(sx1, props?.sx, isMobile && sx3)).toEqual([sx1]);
+  });
+});
