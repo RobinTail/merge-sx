a1a34cef57f01e2ff6f7753e0812245cd22b1c8c
Anna Bocharova <robin_tail@me.com>
2024-05-04 22:25:30 +0200

    Upgrading vitest to 1.6.0. (#439)
    Instead of #436

--- tools/benchmark.processor.ts
+++ tools/benchmark.processor.ts
@@ -2,11 +2,10 @@ import { readFileSync } from "node:fs";
 
 const text = readFileSync("bench.json", "utf-8");
 const json = JSON.parse(text);
-const suite = Object.keys(json.testResults)[0];
+const data = json.files[0].groups[0];
+const suite = data.fullName.split(">")[1].trim();
 
-const results = (
-  json.testResults[suite] as { name: string; hz: number }[]
-).reduce(
+const results = (data.benchmarks as { name: string; hz: number }[]).reduce(
   (agg, { name, hz }) => ({
     ...agg,
     [suite]: { ...agg[suite], [name]: Math.round(hz) },
