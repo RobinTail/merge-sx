dacef968b1b2c332d395701d93eebc8a52c611cc
Robin Tail <robin_tail@me.com>
2024-05-08 21:22:40 +0200

    Building and integration testing commands.

---
+++ build.ts
@@ -0,0 +1,11 @@
+Bun.build({
+  entrypoints: ["src/index.ts"],
+  outdir: "dist",
+  /** @link https://github.com/oven-sh/bun/issues/159 */
+  format: "esm",
+  splitting: false,
+  sourcemap: "none",
+  minify: true,
+  // dts: true,
+  target: "browser",
+});
