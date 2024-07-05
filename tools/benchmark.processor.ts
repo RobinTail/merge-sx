553dfe3588b2d0e6a79d77e8baff82327e2f17ef
Robin Tail <robin_tail@me.com>
2024-07-05 20:46:53 +0200

    Reading file using Bun.file() instead of node:fs.

--- tools/benchmark.processor.ts
+++ tools/benchmark.processor.ts
@@ -1,6 +1,4 @@
-import { readFileSync } from "node:fs";
-
-const text = readFileSync("bench.json", "utf-8");
+const text = await Bun.file("bench.json", { type: "application/json" }).json();
 const json = JSON.parse(text);
 const data = json.files[0].groups[0];
 const suite = data.fullName.split(">")[1].trim();
