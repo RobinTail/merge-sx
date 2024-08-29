2ee04378504feda2979d5eac1e74a82fca10c34b
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-29 17:11:01 +0100

    feat: `Bun.build` -> `tsdown(rolldown)` (#511)
    Hi!
    I'm the author of
    [bun-plugin-isolated-decl](https://github.com/ryoppippi/bun-plugin-isolated-decl)
    I recently tried `tsdown` and it is better than `Bun.build`.
    So this is just an introduction to you.
    If you do not want to use it, just close this PR!
    
    ---------
    
    Co-authored-by: Robin Tail <robin_tail@me.com>

---
+++ tsdown.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: ["./src/index.ts"],
+  outDir: "dist",
+  format: ["esm", "cjs"],
+  clean: true,
+  platform: "neutral",
+  dts: true,
+  minify: true,
+});
