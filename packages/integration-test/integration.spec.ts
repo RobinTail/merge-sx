ee9f9d4118a4f7615eb0356a5030a53e664629e4
Anna Bocharova <anna.bocharova@grover.com>
2025-10-13 15:15:07 +0200

    Updating int test.

--- packages/integration-test/integration.spec.ts
+++ packages/integration-test/integration.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "bun:test";
-import type { SxProps, Theme } from "@mui/system";
+import type { SxProps, Theme } from "@mui/material";
 import { mergeSx } from "merge-sx";
 
 describe("Integration", () => {
